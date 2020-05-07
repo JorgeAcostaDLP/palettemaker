ce20cb97f16fce11be74f5e3d3c74ef7ea3da10f
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-05-07 14:01:07 -0400

    added footer with emoji

--- src/Palette.js
+++ src/Palette.js
@@ -17,10 +17,13 @@ class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name}></ColorBox>
+      <ColorBox
+        key={color.id}
+        background={color[format]}
+        name={color.name}></ColorBox>
     ));
     return (
       <div className='Palette'>
@@ -29,7 +32,10 @@ class Palette extends Component {
           handleChange={this.changeFormat}
           changeLevel={this.changeLevel}></Navbar>
         <div className='Palette-colors'>{colorBoxes}</div>
-        {/*footer*/}
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     );
   }
