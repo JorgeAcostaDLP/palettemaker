afb9587b363f225d2584f1d32d5fb7af39f0a59b
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-05-07 17:09:55 -0400

    Style miniPalette 1

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
   render() {
     const miniPalettes = this.props.colors.map((palette) => (
-      <li>
-        <Link to={`palette/${palette.id}`}> {palette.paletteName}</Link>
-      </li>
+      <MiniPalette {...palette}></MiniPalette>
     ));
     return (
       <div>
-        <MiniPalette></MiniPalette>
+        {' '}
+        <h1>REKTCOLORS!</h1>
         {miniPalettes}
       </div>
     );
