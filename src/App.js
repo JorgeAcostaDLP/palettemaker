f5094ff235631530f842773001b97eaadc7501c8
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-05-07 14:10:08 -0400

    added router dom, switch and route and also broke the thing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import generatePalette from './colorHelpers';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => <h1>PALETTE LIST GOES HERE</h1>}></Route>
+        <Route
+          exact
+          path='/palette/:id'
+          render={() => <h1>iNDIVIDUAL palette</h1>}></Route>
+      </Switch>
+      /* <div>
         <Palette palette={generatePalette(seedColors[4])}></Palette>
-      </div>
+      </div> */
     );
   }
 }
